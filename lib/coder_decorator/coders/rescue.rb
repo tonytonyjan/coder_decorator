0a67c38200b5057c3c0b93eaabb806f90d41b0b5
Jian Weihang <tonytonyjan@gmail.com>
2016-12-11 02:37:15 +0800

    add rescue decorator

---
+++ lib/coder_decorator/coders/rescue.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+require 'coder_decorator/coders/coder'
+module CoderDecorator
+  module Coders
+    # When there's exception raised, it rescues and return +nil+.
+    class Rescue < Coder # :nodoc:
+      def encode(obj)
+        coder.encode(obj)
+      end
+
+      def decode(obj)
+        coder.decode(obj)
+      rescue
+        nil
+      end
+    end
+  end
+end
