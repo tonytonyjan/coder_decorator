13025a6b5e0daedf120ec03608b9d5b39990601c
Jian Weihang <tonytonyjan@gmail.com>
2017-03-09 11:00:11 +0800

    support ruby 2.4.0

--- test/test_cipher.rb
+++ test/test_cipher.rb
@@ -5,7 +5,7 @@ require 'coder_decorator/coders/cipher'
 class TestCipher < Minitest::Test
   include CoderDecorator
   def setup
-    @coder = Coders::Cipher.new secret: 'x' * 100
+    @coder = Coders::Cipher.new secret: 'x' * 32
   end
 
   def test_it_works
@@ -22,10 +22,11 @@ class TestCipher < Minitest::Test
 
   def test_key_rotation
     str = 'hello world'
-    secret = 'x' * 100
+    secret = 'x' * 32
+    new_secret = 'y' * 32
     coder = Coders::Cipher.new secret: secret
     encoded = coder.encode(str)
-    new_coder = Coders::Cipher.new secret: 'new secret', old_secret: secret
+    new_coder = Coders::Cipher.new secret: new_secret, old_secret: secret
     assert_equal str, new_coder.decode(encoded)
   end
 end
